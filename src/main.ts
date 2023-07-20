d2948725626fc69eb2682c5bdece4af06abf2e7f
System Administrator <root@MacBook-Air-Denis.local>
2023-07-21 00:30:23 +0300

    Добавлен CORS для локалхоста приложения на фронте

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Здесь указываются разрешенные HTTP методы
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Здесь указываются разрешенные заголовки запроса
+  });
+
   await app.listen(3000);
 }
 bootstrap();
